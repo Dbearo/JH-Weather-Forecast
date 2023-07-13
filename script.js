971b103878399452cc25339a392a935e5b26d0bf
Jude Hartmann <jude.hartmann@outlook.com>
2023-07-13 16:10:24 -0400

    added the search and weather functionality

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-
+// var day1 = $('#forecasts-cards-container .future-card:eq(0)')
+// var day2 = $('#forecasts-cards-container .future-card:eq(1)')
+// var day3 = $('#forecasts-cards-container .future-card:eq(2)')
+// var day4 = $('#forecasts-cards-container .future-card:eq(3)')
+// var day5 = $('#forecasts-cards-container .future-card:eq(4)')
+// $('#forecasts-cards-container .future-card:eq(1) .future-temp');
+// example for when I set the forcast days
 var apikey = "82d5c90ebbd4523a1d69f1cd7bbb8f14"
 // apiurl = "api.openweathermap.org/data/2.5/forecast?q=$"+city+"&appid="
 var apikey2 = "9799df0eb694f5cb50086ad03b3ab084"
-
+var storage = []
 
 
 function setForcast(datas){
-   city = $(".search").val()
-    var lon = datas.coord.lon.toString()
-    var lat = datas.coord.lat.toString()
-      console.log(lon);
-    console.log(lat);
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`, {
+var count = 0;
+for(i=0,i;datas.list.length;i+=8){
+var ftemp = datas.list[i].main.temp
+ftemp -= 273.15
+ftemp = ftemp.toFixed(2);
+$('#forecasts-cards-container .future-card:eq('+count+') .future-temp').text('Temp: '+ftemp+'C');
+$('#forecasts-cards-container .future-card:eq('+count+') .future-humidity').text('Humidity: '+datas.list[i].main.humidity+"%");
+$('#forecasts-cards-container .future-card:eq('+count+') .future-wind').text('Wind Speed: '+datas.list[i].wind.speed+'MPS');
+var ficonCode = datas.list[i].weather[0].icon
+var ficonurl = "http://openweathermap.org/img/w/" + ficonCode + ".png";
+$('#forecasts-cards-container .future-card:eq('+count+') .future-icon').attr('src', ficonurl);
+count++
+futureDay = new dayjs().add(count,'d')
+count--
+$('#forecasts-cards-container .future-card:eq('+count+') .future-date').text(futureDay);
+count++
+}}
+
+function setCurrent(datas){
+  var temp = datas.main.temp
+  temp -= 273.15
+  temp = temp.toFixed(2);
+  console.log(temp)
+  $('.temp').text("Temp: "+temp+"C" )
+  $('.humidity').text("Humidity: "+datas.main.humidity+"%")
+  $('.wind').text("Wind Speed: "+datas.wind.speed+"MPS")
+  $('.name').text(datas.name)
+  var currentDay = dayjs().format('dddd, MMMM D');
+  $(".date").text(currentDay)
+  var iconCode = datas.weather[0].icon
+  console.log(iconCode)
+  var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+  $('.icon').attr('src', iconurl);
+
+}
+
+function weathers(city) {
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ apikey, {
 
   })
     .then(response => response.json())
@@ -19,6 +57,8 @@ function setForcast(datas){
 
 
       console.log(data)
+
+      setCurrent(data)
     })
  
 
@@ -26,20 +66,11 @@ function setForcast(datas){
       // Handle any errors
       console.error(error);
     });
-}
-
-
-$("#searchbtn").click( function(){
-
-  var city = $(".search").val()
-  console.log(city)
-fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ apikey, {
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`, {
 
   })
     .then(response => response.json())
     .then(data =>{
-
-
       console.log(data)
       setForcast(data)
     })
@@ -49,12 +80,25 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ apike
       // Handle any errors
       console.error(error);
     });
-  
 
+
+}
+
+
+
+$("#searchbtn").click( function(){
+
+  var city = $(".search").val()
+  console.log(city)
+  weathers(city);
+  var history = $('<li>').text(city);
+ 
+  $('#history').append(history);
     })
 
-// Target the first child of the div
-$('#forecasts-cards-container .future-card:eq(0)').css('color', 'red');
+$(document).on("click",'li',function(){
+var city = $(this).val()
+console.log(city)
+weathers(city);
 
-// Target the second child of the div
-$('#forecasts-cards-container .future-card:eq(1)').css('color', 'blue');
+})
